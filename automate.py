618eff86fc47f2c8b97c3fad55cb913fa2125a1c
ejiro osiephri <osiephriejiro765@gmail.com>
2024-08-13 16:39:29 +0100

    feats/add automate everything with python

---
+++ automate.py
@@ -0,0 +1,36 @@
+import sys
+print(sys.version)
+print('hello world')
+
+if 5 > 2:
+    print('5 is greater than 2')
+if 5 > 2:
+    print('5 is greater than 2')
+    
+a = 200
+b = 'ejiro'
+c = True
+print(type(a))
+print(type(b))
+print(type(c))
+
+x = int(3)
+y = str(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x , y, z = 'ejiro', 'is', 'awesome'
+print(x,y,z)
+
+x = y = z = 'ejiro'
+
+print(x)
+
+fruits = ['apple', 'banana', 'cherry']
+
+x,y,z = fruits
+
+print(x,y,z)
