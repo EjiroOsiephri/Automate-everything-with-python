00f82be620b7bd08db9050c2c5018f6df6e817bb
ejiro osiephri <osiephriejiro765@gmail.com>
2024-08-19 13:52:37 +0100

    feat/update conditionals

--- automate.py
+++ automate.py
@@ -285,3 +285,12 @@ for x in myFamily:
 
 for x, obj in myFamily.items():
     print(x, obj)
+
+
+a = 33
+b = 33
+
+if b > a:
+    print('b is greater than a')
+elif a == b:
+    print('a and b are equal')
